c2d9bb0883fe4d37ae62337920e0823e8935ab2d
cr00z <netrebinr@gmail.com>
2019-04-08 21:25:00 +0700

    make first attempt

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+It's a simple script to watch of friends "online" in VKontakte social network.
 
 # How to Install
+
+For work you need to register our application in [VK Developers](https://vk.com/dev) and receive APP_ID. After that insert it into script:
+
+```python
+APP_ID = 1234567
+```
+
+Also you need input authentication data of your personal account in VKontakte (login and password).
 
 Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
 
@@ -11,6 +19,17 @@ pip install -r requirements.txt # alternatively try pip3
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+```bash
+
+$ python vk_friends_online.py
+User login: +79777777777
+User password: vkcom123
+Александров Александр
+Иванов Иван
+Сергеев Сергей
+
+```
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,46 @@
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6934498
 
 
 def get_user_login():
-    pass
+    return input("User login: ")
 
 
 def get_user_password():
-    pass
+    return input("User password: ")
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+        )
+        api = vk.API(session, v='5.0')
+        friends_all = api.friends.get(fields='online')
+    except Exception:
+        friends_online = None
+    else:
+        friends_online = []
+        for friend in friends_all['items']:
+            #if friend['online']:
+                friend_info = '{} {}'.format(friend['last_name'], friend['first_name'])
+                friends_online.append(friend_info)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print(friend)
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
     friends_online = get_online_friends(login, password)
+    if friends_online is None:
+        exit('Vk API Error')
     output_friends_to_console(friends_online)

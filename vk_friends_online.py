bd29cece34af15d606cccc0ed587db345df5a7b1
cr00z <netrebinr@gmail.com>
2019-04-09 15:35:16 +0700

    make second attempt

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,18 +1,20 @@
 import vk
+import getpass
 
 
 APP_ID = 6934498
+API_VERSION = '5.92'
 
 
 def get_user_login():
-    return input("User login: ")
+    return input('User login: ')
 
 
 def get_user_password():
-    return input("User password: ")
+    return getpass.getpass(prompt='User password: ')
 
 
-def get_online_friends(login, password):
+def get_online_friends(login, password, api_version):
     try:
         session = vk.AuthSession(
             app_id=APP_ID,
@@ -20,7 +22,7 @@ def get_online_friends(login, password):
             user_password=password,
             scope='friends'
         )
-        api = vk.API(session, v='5.92')
+        api = vk.API(session, v=api_version)
         friends_online_ids = api.friends.getOnline()
         friends_online = api.users.get(user_ids=friends_online_ids)
     except vk.exceptions.VkAuthError:
@@ -29,6 +31,8 @@ def get_online_friends(login, password):
 
 
 def output_friends_to_console(friends_online):
+    if len(friends_online) == 0:
+        print('No friends online')
     for friend in friends_online:
         print('{} {}'.format(friend['last_name'], friend['first_name']))
 
@@ -36,7 +40,7 @@ def output_friends_to_console(friends_online):
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    friends_online = get_online_friends(login, password, API_VERSION)
     if friends_online is None:
         exit('Vk Authorization Error')
     output_friends_to_console(friends_online)
